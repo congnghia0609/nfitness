82e357ccadbf8a6d792b5d56c2d94864f933994c
congnghia0609 <congnghia0609@gmail.com>
2020-08-27 12:15:01 +0700

    add screen Home

---
+++ lib/constants.dart
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 nghiatc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+///
+/// @author nghiatc
+/// @since Aug 27, 2020
+
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFF8F8F8);
+const kActiveIconColor = Color(0xFFE68342);
+const kTextColor = Color(0xFF222B45);
+const kBlueLightColor = Color(0xFFC7B8F5);
+const kBlueColor = Color(0xFF817DC0);
+const kShadowColor = Color(0xFFE6E6E6);
